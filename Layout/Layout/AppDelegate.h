bff0c0d3f1a6d16d8b2fdb45832918feea290520
xiangronghua <xiangronghua@xiangronghuaMac.local>
2017-04-20 13:38:54 +0800

    first commit

---
+++ Layout/Layout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Layout
+//
+//  Created by xiangronghua on 2017/4/20.
+//  Copyright © 2017年 xiangronghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Layout/Layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Layout
+//
+//  Created by xiangronghua on 2017/4/20.
+//  Copyright © 2017年 xiangronghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
